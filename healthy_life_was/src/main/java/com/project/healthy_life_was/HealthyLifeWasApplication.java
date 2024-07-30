e23f8898884e5afbb46ad46b385c184b1ce7a23d
devgiants75 <devgiants75@naver.com>
2024-07-30 19:43:41 +0900

    20240730

---
+++ healthy_life_was/src/main/java/com/project/healthy_life_was/HealthyLifeWasApplication.java
@@ -0,0 +1,13 @@
+package com.project.healthy_life_was;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyLifeWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyLifeWasApplication.class, args);
+	}
+
+}
---
+++ healthy_life_was/src/test/java/com/project/healthy_life_was/HealthyLifeWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.healthy_life_was;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyLifeWasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
